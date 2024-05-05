078d8c5eaaa2afb4ea40e963d39a5811a9912604
Shaowu Tseng <breeze101792@gmail.com>
2024-05-05 10:57:15 +0800

    [hal] Fix mac os read data
    [Description]
    Fix mac os read data
    
    [Self-Test]
    N/A

--- cliphal/macclip.py
+++ cliphal/macclip.py
@@ -16,8 +16,10 @@ class MacClip(ClipBase):
         process.communicate(output.encode('utf-8'))
 
     def read_from_clipboard(self):
+        # return subprocess.check_output(
+        #     'pbpaste', env={'LANG': 'en_US.UTF-8'}).decode('utf-8')
         return subprocess.check_output(
-            'pbpaste', env={'LANG': 'en_US.UTF-8'}).decode('utf-8')
+            'pbpaste', env={'LANG': 'en_US.UTF-8'})
     @staticmethod
     def isSupported():
         if sys.platform == 'darwin':
